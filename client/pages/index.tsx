5e10c5cf7d9fd55a29f40da954bb32b5dbedb5c9
DevOps <DevOps@practice.fr>
2025-07-02 14:01:53 +0000

    v3.8.25

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -207,7 +207,7 @@ const Home: NextPage = () => {
         <div className={styles.version}>
           <Footer className="font-semibold leading-5 opacity-50" />
 
-          <div>v{process.env.appVersion} by Alexandre Domont 2025/06/30 </div>
+          <div>v{process.env.appVersion} by Alexandre Domont 2025/07/02 </div>
         </div>
 
         <div className={styles.actions}>
